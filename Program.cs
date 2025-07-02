27e412d9c1cc344528460b9995ed3be7a0c4664d
rhommardev2023 <rjmlp02@gmail.com>
2025-07-01 23:02:16 -0700

    ejemplo de operador condicional

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection.Metadata;
+
+// Título: Condicionales en C#
+
+//paso  1 declaración de variables
+int quantidadeEnEstoque = 10;
+int quantidadeCompra = 4;
+
+
+
+
+
+//paso 2 bloque de codigo condicional
+if (quantidadeEnEstoque >= quantidadeCompra) // si quantidade estoque es mayor o igual a quantidade compra
+{
+    Console.WriteLine("Venda realizada"); //impreime mensaje de venta realizada
+}
+else // si no se cumple la condicion
+{
+    Console.WriteLine("Desculpe. não temos estoque suficiente"); //imprime este mensaje
+}
+
+
+//ejemplo 2  
+//OPERADORES LOGICOS
+
+//OPERADOR OR 
+
